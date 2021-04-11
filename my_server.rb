cd14d285f03b30698b77154023e8d65cf08eea1e
wei chen <weichen9083@gmail.com>
2021-04-11 02:37:54 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -1,2 +1,9 @@
-require_relative './my_server'
-run MyServer.new
+class MyServer
+  def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+  end
+ 
+  def pretty_response
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+  end
+end
